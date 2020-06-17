877d583cae688f4b425d8d4c41ac7ba4498a91c8
Lucas Yoshimoto <lucasyoshimoto238@gmail.com>
2020-06-17 14:57:23 -0300

    Consulta simples com query methods

--- src/main/java/com/lucasyoshimoto/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/lucasyoshimoto/workshopmongo/repository/PostRepository.java
@@ -1,5 +1,7 @@
 package com.lucasyoshimoto.workshopmongo.repository;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,4 +10,5 @@ import com.lucasyoshimoto.workshopmongo.domain.Post;
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
 
+	List<Post> findByTitleContainingIgnoreCase(String text);
 }
